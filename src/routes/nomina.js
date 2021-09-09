b7311ba2261491d27dc6736d90342cb89fc79a5b
Ele Cabrera <jai690@hotmail.com>
2021-09-09 11:07:57 -0500

    ordenamiento

--- src/routes/nomina.js
+++ src/routes/nomina.js
@@ -6,8 +6,10 @@ const router = express.Router();
 const pool = require('../database');
 
 router.get('/', async(req,res) => {
+    const ejecutivos = await pool.query('SELECT * FROM users WHERE rol = "Ejecutivo"');
+    console.log(ejecutivos);
     const operaciones = await pool.query('SELECT * FROM operacion LEFT JOIN trabajador ON operacion.trabajadorId=trabajador.id');
-    res.render("../views/nomina/index.hbs", {operaciones});
+    res.render("../views/nomina/index.hbs", {operaciones, ejecutivos});
 });
 
 module.exports = router;
