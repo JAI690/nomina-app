8bb8fc3bea74c228265e533d8a7c735429344695
Ele Cabrera <jai690@hotmail.com>
2021-09-20 15:04:46 -0500

    formato fecha

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -1,5 +1,6 @@
 const helpers = {};
 const hb = require('handlebars');
+const moment = require("moment");
 
 hb.registerHelper('status', function (value) {
     if(value === '1'){
@@ -8,6 +9,11 @@ hb.registerHelper('status', function (value) {
         return false;
     }
     
+  });
+  
+hb.registerHelper('dateFormat', function (date, options) {
+      const formatToUse = (arguments[1] && arguments[1].hash && arguments[1].hash.format) || "DD/MM/YYYY"
+      return moment(date).format(formatToUse);
   });
 
 module.exports = helpers;
